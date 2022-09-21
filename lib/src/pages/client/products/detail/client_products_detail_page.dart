b3cd7a568d7e817b16579880741d47f8a7c4faf7
EdgarG15 <edgarjhdez1501@gmail.com>
2022-09-21 11:12:48 -0600

    commit

--- lib/src/pages/client/products/detail/client_products_detail_page.dart
+++ lib/src/pages/client/products/detail/client_products_detail_page.dart
@@ -1,14 +1,163 @@
+import 'package:app_delivery/src/models/product.dart';
 import 'package:app_delivery/src/pages/client/products/detail/client_product_detail_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 import 'package:get/get.dart';
 
 class ClientProductsDetailPage extends StatelessWidget {
+  Product? product;
   ClientProductDetailController con = Get.put(ClientProductDetailController());
+
+  ClientProductsDetailPage({@required this.product});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text(('producto'))),
+      bottomNavigationBar: Container(height: 100, child: _buttonsAddToBag()),
+      body: Column(
+        children: [
+          _imageSlideShow(context),
+          _textNameProduct(),
+          _textDescriptionProduct(),
+          _textPriceProduct(),
+        ],
+      ),
+    );
+  }
+
+  Widget _textNameProduct() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.only(top: 30, left: 30, right: 30),
+      child: Text(
+        product?.name ?? '',
+        style: TextStyle(
+            fontWeight: FontWeight.bold, fontSize: 22, color: Colors.black),
+      ),
+    );
+  }
+
+  Widget _textDescriptionProduct() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.only(top: 20, left: 30, right: 30),
+      child: Text(
+        product?.description ?? '',
+        style: TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  Widget _textPriceProduct() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.only(top: 10, left: 30, right: 30),
+      child: Text(
+        '\$${product?.price.toString() ?? ''}',
+        style: TextStyle(
+            fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget _buttonsAddToBag() {
+    return Column(
+      children: [
+        Divider(
+          height: 1,
+          color: Colors.grey[400],
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 20, right: 20, top: 15),
+          child: Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    minimumSize: const Size(45, 37),
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(25),
+                            bottomLeft: Radius.circular(25)))),
+                child: const Text(
+                  '-',
+                  style: TextStyle(color: Colors.black, fontSize: 22),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    minimumSize: const Size(40, 37)),
+                child: const Text(
+                  '0',
+                  style: TextStyle(color: Colors.black, fontSize: 22),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    minimumSize: const Size(45, 37),
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(25),
+                            bottomRight: Radius.circular(25)))),
+                child: const Text(
+                  '+',
+                  style: TextStyle(color: Colors.black, fontSize: 22),
+                ),
+              ),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.amber,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25))),
+                child: Text(
+                  'Agregar ${product?.price ?? ''}',
+                  style: const TextStyle(color: Colors.black, fontSize: 22),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _imageSlideShow(BuildContext context) {
+    return ImageSlideshow(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.4,
+      initialPage: 0,
+      indicatorColor: Colors.amber,
+      indicatorBackgroundColor: Colors.grey,
+      children: [
+        FadeInImage(
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 50),
+            placeholder: AssetImage('assets/img/no_image.jpg'),
+            image: product!.image1 != null
+                ? NetworkImage(product!.image1!)
+                : AssetImage('assets/img/no_image.jpg') as ImageProvider),
+        FadeInImage(
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 50),
+            placeholder: AssetImage('assets/img/no_image.jpg'),
+            image: product!.image2 != null
+                ? NetworkImage(product!.image2!)
+                : AssetImage('assets/img/no_image.jpg') as ImageProvider),
+        FadeInImage(
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 50),
+            placeholder: AssetImage('assets/img/no_image.jpg'),
+            image: product!.image3 != null
+                ? NetworkImage(product!.image3!)
+                : AssetImage('assets/img/no_image.jpg') as ImageProvider),
+      ],
     );
   }
 }
