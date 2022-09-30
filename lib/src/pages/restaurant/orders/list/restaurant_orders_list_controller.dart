0bdcd2765a74e49fc352c93eefee151d84d3e783
EdgarG15 <edgarjhdez1501@gmail.com>
2022-09-30 11:54:21 -0600

    commit

--- lib/src/pages/restaurant/orders/list/restaurant_orders_list_controller.dart
+++ lib/src/pages/restaurant/orders/list/restaurant_orders_list_controller.dart
@@ -1,3 +1,13 @@
+import 'package:app_delivery/src/models/order.dart';
+import 'package:app_delivery/src/providers/orders_provider.dart';
 import 'package:get/get.dart';
 
-class RestaurantOrdersListController extends GetxController {}
+class RestaurantOrdersListController extends GetxController {
+  OrdersProvider ordersProvider = OrdersProvider();
+  List<String> status =
+      <String>['PAGADO', 'DESPACHADO', 'EN CAMINO', 'ENTREGADO'].obs;
+
+  Future<List<Order>> getOrders(String status) async {
+    return await ordersProvider.findByStatus(status);
+  }
+}
--- lib/src/pages/restaurant/orders/list/restaurant_orders_list_page.dart
+++ lib/src/pages/restaurant/orders/list/restaurant_orders_list_page.dart
@@ -1,6 +1,9 @@
+import 'package:app_delivery/src/models/order.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../../utils/relative_time_util.dart';
+import '../../../../widgets/no_data_widget.dart';
 import 'restaurant_orders_list_controller.dart';
 
 class RestaurantOrdersListPage extends StatelessWidget {
@@ -9,9 +12,122 @@ class RestaurantOrdersListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Restaurant Orders List'),
+    return Obx(
+      () => DefaultTabController(
+        length: con.status.length,
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(50),
+            child: AppBar(
+                bottom: TabBar(
+              isScrollable: true,
+              indicatorColor: Colors.amber,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey[600],
+              tabs: List<Widget>.generate(con.status.length, (index) {
+                return Tab(
+                  child: Text(con.status[index]),
+                );
+              }),
+            )),
+          ),
+          body: Center(
+            child: TabBarView(
+              children: con.status.map((String status) {
+                return FutureBuilder(
+                    future: con.getOrders(status),
+                    builder: (context, AsyncSnapshot<List<Order>> snapshot) {
+                      if (snapshot.hasData) {
+                        if (snapshot.data!.length > 0) {
+                          return ListView.builder(
+                              itemCount: snapshot.data?.length ?? 0,
+                              itemBuilder: (_, index) {
+                                return _cardOrder(snapshot.data![index]);
+                              });
+                        } else {
+                          return Center(
+                            child: NoDataWidget(
+                              text: 'No hay ordenes',
+                            ),
+                          );
+                        }
+                      } else {
+                        return Center(
+                          child: NoDataWidget(
+                            text: 'No hay ordenes',
+                          ),
+                        );
+                      }
+                    });
+              }).toList(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _cardOrder(Order order) {
+    return Container(
+      height: 150,
+      margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+      child: Card(
+        elevation: 3.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              height: 30,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+              ),
+              child: Container(
+                margin: const EdgeInsets.only(top: 5),
+                child: Text(
+                  'Orden #${order.id}',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 15, left: 20, right: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 5),
+                    width: double.infinity,
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                        'Pedido: ${RelativeTimeUtil.getRelativeTime(order.timestamp ?? 0)}'),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 5),
+                    width: double.infinity,
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                        'Cliente: ${order.client?.name ?? ''} ${order.client?.lastname ?? ''}'),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 5),
+                    width: double.infinity,
+                    alignment: Alignment.centerLeft,
+                    child: Text('Entregar en: ${order.address?.address ?? ''}'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
